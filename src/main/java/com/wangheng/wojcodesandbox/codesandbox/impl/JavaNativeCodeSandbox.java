6454d135bfc9b67985050cd00999f6412b6a04c6
SaltedFish <1720466861@qq.com>
2024-05-09 21:09:48 +0800

    使用CountDownLatch改进多线程执行java代码

--- src/main/java/com/wangheng/wojcodesandbox/codesandbox/impl/JavaNativeCodeSandbox.java
+++ src/main/java/com/wangheng/wojcodesandbox/codesandbox/impl/JavaNativeCodeSandbox.java
@@ -9,7 +9,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class JavaNativeCodeSandbox extends JavaCodeSandboxTemplate {
@@ -23,28 +26,31 @@ public class JavaNativeCodeSandbox extends JavaCodeSandboxTemplate {
             new ArrayBlockingQueue<>(20),
             new ThreadPoolExecutor.CallerRunsPolicy());
     @Override
-    public List<ExecuteMessage> runFile(File userCodeFile, List<String> inputList) throws InterruptedException, ExecutionException {
+    public List<ExecuteMessage> runFile(File userCodeFile, List<String> inputList) throws InterruptedException{
         int N=inputList.size();
-        List<Future<ExecuteMessage>> futures=new ArrayList<>(N);
+        List<ExecuteMessage> runMessageList = new ArrayList<>(N);
+        CountDownLatch countDownLatch = new CountDownLatch(inputList.size());
         for(int i=0;i<inputList.size();i++){
             int finalI = i;
-            Future<ExecuteMessage> future=pool.submit(()->{
-                ExecuteMessage runMessage = ProcessUtils.runInteractProcessAndGetMessage(
-                        String.format(
-                                "java -Xmx256m -Dfile.encoding=UTF-8 -cp %s;%s -Djava.security.manager=%s Main",
-                                userCodeFile.getParent(),
-                                SECURITY_MANAGER_PATH,
-                                SECURITY_MANAGER_CLASS_NAME),
-                        "运行",inputList.get(finalI)
-                );
-                return runMessage;
+            pool.submit(()->{
+                ExecuteMessage runMessage = null;
+                try {
+                    runMessage = ProcessUtils.runInteractProcessAndGetMessage(
+                            String.format(
+                                    "java -Xmx256m -Dfile.encoding=UTF-8 -cp %s;%s -Djava.security.manager=%s Main",
+                                    userCodeFile.getParent(),
+                                    SECURITY_MANAGER_PATH,
+                                    SECURITY_MANAGER_CLASS_NAME),
+                            "运行",inputList.get(finalI)
+                    );
+                } catch (IOException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                runMessageList.add(finalI,runMessage);
+                countDownLatch.countDown();
             });
-            futures.add(i,future);
         }
-        List<ExecuteMessage> runMessageList = new ArrayList<>(N);
-        for (Future<ExecuteMessage> future : futures) {
-            runMessageList.add(future.get());
-        }
+        countDownLatch.await();
         return runMessageList;
     }
 }
